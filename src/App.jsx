f4718375e025c79ecb437848e98bb0d1686200af
justpeterjp1 <oluwagbotemiojopeter10@gmail.com>
2026-04-02 06:46:26 +0100

    added add, delete and other UI updating logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
+import SelectedProject from "./components/SelectedProject";
 
 
 function App() {
@@ -9,7 +10,16 @@ function App() {
          selectedProjectId: undefined,
          projects: []
      });
- 
+     
+     function handleSelectProject(id) {
+        setProjectState((prevState) => {
+             return {
+               ...prevState,
+                 selectedProjectId: id,
+             }
+         });
+     }
+
      const handleStartAddProject = () => {
          setProjectState((prevState) => {
              return {
@@ -40,8 +50,19 @@ function App() {
         }
       })
      }
-     
-     let content;
+
+     function handleDeleteProject() {
+      setProjectState(prevState => {
+        return {
+          ...prevState,
+          selectedProjectId: undefined,
+          projects: prevState.projects.filter(p => p.id !== prevState.selectedProjectId)
+        }
+      })
+     }
+
+     const selectedProject = projectState.projects.find(p => p.id === projectState.selectedProjectId);
+     let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
      if (projectState.selectedProjectId === null) {
          content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
      } else if(projectState.selectedProjectId === undefined) {
@@ -53,6 +74,7 @@ function App() {
   return (
     <main className="h-screen flex my-8 gap-8">
       <Sidebar
+      onSelectProject={handleSelectProject}
       projects={projectState.projects} 
       onStartAddProject={handleStartAddProject} />
         {content}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Button from "./Button"
-export default function Sidebar({ onStartAddProject, projects }) {
+export default function Sidebar({ onStartAddProject, projects, onSelectProject, selectedProjectId }) {
     
 
     return <aside id="sidebar" className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl uppercase">
@@ -11,9 +11,21 @@ export default function Sidebar({ onStartAddProject, projects }) {
             className="text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100 mb-4" >+ Add Project</Button>
         </p>
         <ul className="mt-8">
-            {projects.map(project => <li key={project.id}>
-                <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
-            </li>)}
+            {projects.map((project) => {
+                let cssClasses = ""
+                if (project.id === selectedProjectId) {
+                    cssClasses += " bg-stone-800 text-stone-200"
+                } else {
+                    cssClasses += " text-stone-400"
+                }
+                return (<li key={project.id}>
+                <button 
+                    onClick={() => onSelectProject(project.id)}
+                    className={`w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 ${cssClasses}`}>
+                    {project.title}
+                </button>
+
+            </li>)} )}
         </ul>
     </aside>
 }
